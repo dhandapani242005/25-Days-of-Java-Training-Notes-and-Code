9fdd8999c44e85053d20e9b1d4c083f3d35f9e09
Dhandapani M <157560061+dhandapani242005@users.noreply.github.com>
2025-01-28 13:32:37 +0530

    Add files via upload

---
+++ Pattern Sums/Pattern1.java
@@ -0,0 +1,13 @@
+public class Pattern1 {
+    public static void main(String args[]){
+        int n = 4 ; 
+        int k = 7 ;
+        for(int i=0; i<n; i++){
+            for (int j=0; j<=i; j++){
+                System.out.print(" "+k+++" " );
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
---
+++ Pattern Sums/Pattern6.java
@@ -0,0 +1,12 @@
+public class Pattern6 
+{
+    public static void main(String args[])
+{
+    int n = 7;
+    int s = 1;
+    //for (int i = 0; i < n; i++){
+     //   System.out.print(s);
+    //}
+    
+}    
+}
